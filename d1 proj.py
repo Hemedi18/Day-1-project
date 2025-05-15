674d17ffe575c59de8f47b6768370c5bdf4d1110
Hemedi18 <hemedimdoe18@gmail.com>
2025-05-15 19:14:08 +0300

    Python projects

---
+++ d1 proj.py
@@ -0,0 +1,4 @@
+print(f"wellcome to band generator ".upper())
+city = input(f"what is the name of city you grew up in? \n")
+pet = input(f"what is your pet's name? \n")
+print(f"your band's name could be {city} {pet} \n".capitalize())
